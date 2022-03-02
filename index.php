c0037cbc7f222455cdf30bc6cd985e291228f275
bbcf741 <40833350+bbcf741@users.noreply.github.com>
2022-03-02 16:20:50 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+    </head>
+
+    <body>
+        <?php
+        echo date("Y年m月d日");
+        echo "<hr>";
+        $i = 8;
+        $j = 7;
+        echo "<h1>你好</h1>";
+        echo "8+7=";
+        echo $i+$j;
+        echo "<br>";
+        echo "<b>八加七等於十五</b>";
+        ?>
+    </body>
+</html>
