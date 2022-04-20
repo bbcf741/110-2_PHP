621c0d7c711efac3074aab89ed052b46ac259fdc
bbcf741 <40833350+bbcf741@users.noreply.github.com>
2022-04-21 05:55:40 +0800

    Add files via upload

---
+++ KTtrip.php
@@ -0,0 +1,76 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="墾丁,旅遊,墾丁三日遊,南灣,南灣三日遊">
+    <title>墾丁三日遊</title>
+    <link rel="icon" href="https://ws.ktnp.gov.tw/001/Upload/photoedittemp/d938167c-21e7-4b66-9e4b-4ec4145a77d2.jpg" type="image/x-icon">
+</head>
+
+<body bgcolor="#6ADBEB">
+<center>
+<h1 style="color:#244F49">墾丁三日遊報名表單！！</h1>
+<hr color="rgb(37,75,106)">
+<div>
+  <h2>行程內容</h2>
+  <p>第一天：楠梓援中國小校門口集合＞墾丁凱撒大飯店＞享受飯店設施</p>
+  <p>第二天：凱薩大飯店吃早餐＞墾丁福華渡假飯店＞享受飯店設施</p>
+  <p>第三天：福華渡假飯店吃早餐＞南灣遊憩區玩水水＞開心回家</p>
+  <a href ='https://kenting.caesarpark.com.tw' target = '_blank'>
+  <img width = '300' height = '200' src='https://live.staticflickr.com/65535/40934602563_8cd50493c3_o.jpg'></a>
+  <a href ='https://www.howard-hotels.com.tw/zh_TW/HotelVacation/121' target = '_blank'>
+  <img width = '300' height = '200' src='https://cf.bstatic.com/xdata/images/hotel/max1024x768/203525411.jpg?k=e9f9b23207ecdb07da0363b2285fcde661fc2a89c959f1d1a7a9c83c8231359d&o=&hp=1'></a>
+  <br>
+  <font color="red" size="5" face="新細明體">
+    <b>點擊圖片看看飯店裡面有什麼好玩的！耶！</b>
+  </font>
+</div>
+<hr color="white" width="50%">
+<h2>好想參加！！趕快填表！！</h2>
+<form action="index.php" method="post">
+    姓名：<input type="text" name="name" required> </br>
+    電子信箱：<input type="email" name="email" required> </br>
+    手機號碼：<input type="tel" name="tel" required> </br>
+    性別：
+    <input name="gender" type="radio" required>男
+    <input name="gender" type="radio">女
+    <input name="gender" type="radio">其他</br>
+    飲食偏好：
+    <input type="checkbox" name="food">葷
+    <input type="checkbox" name="food">素
+    <input type="checkbox" name="food">不蛋
+    <input type="checkbox" name="food">不奶
+    <input type="checkbox" name="food">不堅果
+    <input type="checkbox" name="food">不海鮮</br>
+    T-shirt尺寸：
+    <select>
+    <option>XS</option>
+    <option>S</option>
+    <option>M</option>
+    <option>L</option>
+    <option>XL</option>
+    <option>2XL</option>
+    <option>3XL</option>
+    </select></br>
+    T-shirt顏色：<input type="color" required> </br>
+    生日：<input type="date" required> </br>
+    總人數：<input type="number" min="1" required> </br></br>
+    <input type="submit">
+</form>
+<hr color="white" width="50%">
+<h4>T-shirt Size對照表（單位：公分）</h4>
+    <table border="1">
+        <tr><td>尺碼</td><td>衣長</td><td>衣寬</td><td>袖長</td><td>肩寬</td></tr>
+        <tr><td>XS</td><td>62</td><td>41</td><td>18</td><td>40</td></tr>
+        <tr><td>S</td><td>64</td><td>44</td><td>19</td><td>42</td></tr>
+        <tr><td>M</td><td>67</td><td>48</td><td>20</td><td>43</td></tr>
+        <tr><td>L</td><td>71</td><td>51</td><td>21</td><td>45</td></tr>
+        <tr><td>XL</td><td>73</td><td>54</td><td>22</td><td>48</td></tr>
+        <tr><td>2XL</td><td>76</td><td>58</td><td>24</td><td>50</td></tr>
+        <tr><td>3XL</td><td>78</td><td>63</td><td>25</td><td>52</td></tr>
+    </table>
+<hr color="#011F00">
+<a href="#top">回頁首</a>
+</center>
+</body>
+</html>
